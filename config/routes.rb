6763cc620d5b09a2cb35d94856269ff528f3685e
Sr. Mahecha <lemahecha56@misena.edu.co>
2021-07-22 17:02:37 -0500

    Se Genera Controlador Con las distintas peticiones que puede generar un Usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  
   resource :users, only: [:new, :create]
 
   root 'welcome#index'
